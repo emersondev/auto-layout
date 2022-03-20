b25f3b8361814d652a3d2a1282e5414cf86bda27
Emerson Novais <emersonnovais93@gmail.com>
2022-03-20 07:17:08 -0300

    Initial Commit

---
+++ auto-layout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  auto-layout
+//
+//  Created by Emerson.Novais on 20/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
